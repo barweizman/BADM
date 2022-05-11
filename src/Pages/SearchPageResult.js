004512fd7be36cf4ada593f501e7993ef2dc8a57
David Abenhaim <68952247+ZiteiX@users.noreply.github.com>
2022-05-11 11:45:50 +0300

    added animations

--- src/Pages/SearchPageResult.js
+++ src/Pages/SearchPageResult.js
@@ -2,25 +2,28 @@ import React, { useEffect, useState } from "react";
 import { Divider, Grid } from "@mui/material";
 import { useSelector } from "react-redux";
 
+import AppAnimation from "../Components/AppAnimation";
 import Footer from "../Components/Footer";
 import FreeShipping from "../Components/FreeShippingBar";
 import MyNavbar from "../Components/MyNavbar";
 import WhatsappButton from "../Components/WhatsappButton";
 import ProductsList from "../Components/Products/ProductsList";
 import NewsLetter from "../Components/NewsLetter";
+import ProductSortByOptions from "../Components/ProductSortByOptions";
+import SearchBox from "../Components/SearchBox";
 
 import { getFeaturedProducts } from "../services/serverServices";
 import { getSearchResultProduct } from "../store/reducers/appState";
-import SearchBox from "../Components/SearchBox";
 import theme from "../Constants/theme";
-import NotFound from "../Components/Common/NotFound";
-import ProductSortByOptions from "../Components/ProductSortByOptions";
+
+import SearchFailedAnimation from "../assets/animations/no-result.json"
 
 const HomePageSearchResult = () => {
   const state = useSelector(s => s);
 
   const [featuredProducts, setFeaturedProducts] = useState([]);
   const [sortBy, setSortBy] = useState("");
+  const [startedTyping, setStartedTyping] = useState(false);
   const searchResultProduct = getSearchResultProduct(state);
 
   useEffect(() => {
@@ -32,6 +35,10 @@ const HomePageSearchResult = () => {
     getProducts();
   }, []);
 
+  useEffect(() => {
+    setStartedTyping(true);
+  }, [searchResultProduct])
+
   return (
     <Grid>
       <FreeShipping />
@@ -42,24 +49,20 @@ const HomePageSearchResult = () => {
           mb: theme.spacing(4)
         }}
       />
-      {searchResultProduct.length === 0
-        ? <NotFound />
+      {searchResultProduct.length === 0 && startedTyping 
+        ? <Grid mb={theme.spacing(10)} >
+          <AppAnimation LottieCmp={SearchFailedAnimation} />
+          </Grid>
         : <>
         <ProductSortByOptions sortBy={sortBy} handleSetSortBy={(val) => setSortBy(val)} />
         <ProductsList subTitle=" " products={searchResultProduct} sortBy={sortBy} />
         </>}
       <Divider
-        sx={{
-          mb: theme.spacing(4),
-          mt: theme.spacing(4)
-        }}
+        sx={{ mb: theme.spacing(4), mt: theme.spacing(4)}}
       />
       <ProductsList subTitle="People Also Liked" products={featuredProducts} />
       <Divider
-        sx={{
-          mb: theme.spacing(4),
-          mt: theme.spacing(4)
-        }}
+        sx={{ mb: theme.spacing(4), mt: theme.spacing(4) }}
       />
       <NewsLetter />
       <Footer />
